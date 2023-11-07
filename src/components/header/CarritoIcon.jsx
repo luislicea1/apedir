3884e03d6ab98309cc192465da7d8fbb8f75cdd7
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-07 14:02:51 -0500

    updated package lock json

---
+++ src/components/header/CarritoIcon.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Badge } from "@nextui-org/react";
+import { CartIcon } from "../Icons/CartIcon/CartIcon";
+import { grid_3_col , btn100pc} from "../styles/styles";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+
+// En el componente Carrito
+export default function Carrito({ carrito }) {
+  const [isInvisible, setIsInvisible] = React.useState(false);
+
+  
+  if (!carrito) {
+    return null;
+  }
+  return (
+    <div className="mt-2">
+      <Dropdown>
+        <DropdownTrigger>
+          <div>
+            <Badge
+              color="danger"
+              content={carrito.length}
+              isInvisible={isInvisible}
+              shape="circle"
+            >
+              <CartIcon size={30} />
+            </Badge>
+          </div>
+        </DropdownTrigger>
+        <DropdownMenu
+          aria-label="Action event example"
+          
+          className="no-hover"
+        >
+          {carrito.map((product, index) => (
+            <DropdownItem key={index}>
+              <div style={grid_3_col}>
+                <img src={product.image} alt={product.title} width="50px" />
+                <span>{product.title}</span>
+                <span>{product.quantity}</span>
+              </div>
+            </DropdownItem>
+          ))}
+          <DropdownItem>
+            <a href="/carrito">
+              <Button color="primary" style={btn100pc}>
+                Comprar
+              </Button>
+            </a>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+}
---
+++ src/components/header/Notification.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import {
+    Dropdown,
+    DropdownTrigger,
+    DropdownMenu,
+    DropdownItem,
+    Badge,
+} from "@nextui-org/react";
+import { NotificationIcon } from "../Icons/NotificationIcon";
+
+export default function Notification() {
+    const items = [
+        {
+            key: "new",
+            label: "New file",
+        },
+        {
+            key: "copy",
+            label: "Copy link",
+        },
+        {
+            key: "edit",
+            label: "Edit file",
+        },
+        {
+            key: "delete",
+            label: "Delete file",
+        },
+    ];
+    const [isInvisible, setIsInvisible] = useState(false);
+
+    return (
+        <Dropdown>
+            <DropdownTrigger>
+                <div style={{ marginTop: '10px' }}>
+                    <Badge
+                        color="danger"
+                        content={5}
+                        isInvisible={isInvisible}
+                        shape="circle"
+                    >
+                        <NotificationIcon className="fill-current" size={30} color="" />
+                    </Badge>
+                </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Dynamic Actions" items={items}>
+                {(item) => (
+                    <DropdownItem
+                        key={item.key}
+                        color={item.key === "delete" ? "danger" : "default"}
+                        className={item.key === "delete" ? "text-danger" : ""}
+                    >
+                        {item.label}
+                    </DropdownItem>
+                )}
+            </DropdownMenu>
+        </Dropdown>
+    );
+}
