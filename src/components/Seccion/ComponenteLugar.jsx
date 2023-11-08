d525b40b8cf98a0125bfda7bba07c7869949fb94
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-08 16:39:51 -0500

    preload dashdas

--- src/components/Seccion/ComponenteLugar.jsx
+++ src/components/Seccion/ComponenteLugar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, CardHeader, CardBody, Image ,Link} from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -6,38 +6,42 @@ import { CardStyles, ImgCardStyle, ImgStyle, LogoStyle } from "../styles/styles"
 import Stars from "../Stars/Stars";
 
 export default function ComponenteLugar(props) {
-  const navigate = useNavigate();
-  
-  ComponenteLugar.propTypes = {
-    localizacion: PropTypes.string.isRequired,
-    nombre: PropTypes.string.isRequired,
-    numeroPersonas: PropTypes.number.isRequired,
-    imagen: PropTypes.string.isRequired,
-  };
-  
-  return (
-    <Link onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}>
-      <Card className="py-4" style={CardStyles}>
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">{props.localizacion}</p>
-          {/* <small className="text-default-500">
-            {props.numeroPersonas} personas les gusta este lugar
-          </small> */}
-          <h2 className="font-bold text-large mb-2" style={{fontSize: "25px"}}>{props.nombre}</h2>
-          <Stars readOnly w = {100} rating = {3.5}></Stars>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
-          <Image
-            alt="NextUI hero Image with delay"
-            //alt = "NextUI hero Image"
-            className="object-cover rounded-xl"
-            // src={props.imagen}
-            src={props.imagen}
-            style={LogoStyle}
-            //fallbackSrc="https://via.placeholder.com/300x200"
-          />
-        </CardBody>
-      </Card>
-    </Link>
-  );
+ const navigate = useNavigate();
+ 
+ ComponenteLugar.propTypes = {
+   localizacion: PropTypes.string.isRequired,
+   nombre: PropTypes.string.isRequired,
+   numeroPersonas: PropTypes.number.isRequired,
+   imagen: PropTypes.string.isRequired,
+ };
+
+ useEffect(() => {
+   if (props.imagen) {
+     const link = document.createElement('link');
+     link.rel = 'preload';
+     link.href = props.imagen;
+     link.as = 'image';
+     document.head.appendChild(link);
+   }
+ }, [props.imagen]);
+ 
+ return (
+   <Link onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}>
+     <Card className="py-4" style={CardStyles}>
+       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+         <p className="text-tiny uppercase font-bold">{props.localizacion}</p>
+         <h2 className="font-bold text-large mb-2" style={{fontSize: "25px"}}>{props.nombre}</h2>
+         <Stars readOnly w = {100} rating = {3.5}></Stars>
+       </CardHeader>
+       <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
+         <Image
+           alt="NextUI hero Image with delay"
+           className="object-cover rounded-xl"
+           src={props.imagen}
+           style={LogoStyle}
+         />
+       </CardBody>
+     </Card>
+   </Link>
+ );
 }
