8850609f36f1d21fcec858b6af8419451738860e
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-08 15:47:24 -0500

    askdksahdhjhjhjhjhjhjhjassa

--- src/components/Eventos/Eventos.jsx
+++ src/components/Eventos/Eventos.jsx
@@ -1,26 +1,25 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
+import {  Avatar } from "@nextui-org/react";
 import { Link } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
-import { EventoCardStyles, eventoMargin, badge } from "../styles/styles";
-import { Skeleton } from "@nextui-org/react";
 
-const LazyAvatar = lazy(() => import("./LazyAvatar"));
+import { EventoCardStyles, eventoMargin, badge } from "../styles/styles";
 
 export default function Eventos(props) {
   const navigate = useNavigate();
-
+  
   return (
     <div className="" style={eventoMargin}>
       <Link onClick={() => navigate(`/evento/${props.nombre}`)}>
-        <Suspense
-          fallback={
-            <Skeleton className="rounded-lg">
-              <div className="h-24 rounded-lg bg-default-300"></div>
-            </Skeleton>
-          }
-        >
-          <LazyAvatar src={props.imagen} style={EventoCardStyles} />
-        </Suspense>
+        
+          <Avatar
+            isBordered
+            radius="md"
+            color="danger"
+            src={props.imagen}
+            style={EventoCardStyles}
+          />
+        
       </Link>
     </div>
   );
