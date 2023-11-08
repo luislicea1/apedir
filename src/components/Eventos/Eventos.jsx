fd88188726ed39b1f77c1bde2e5812652ead0edb
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-08 16:17:30 -0500

    cambios en el avatar

--- src/components/Eventos/Eventos.jsx
+++ src/components/Eventos/Eventos.jsx
@@ -1,32 +1,25 @@
 import React from "react";
-import { Avatar , Image, Card} from "@nextui-org/react";
+import {  Avatar } from "@nextui-org/react";
 import { Link } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
-
+import MyAvatar from "./Avatar";
 import { EventoCardStyles, eventoMargin, badge } from "../styles/styles";
 
 export default function Eventos(props) {
   const navigate = useNavigate();
-
+  
   return (
     <div className="" style={eventoMargin}>
       <Link onClick={() => navigate(`/evento/${props.nombre}`)}>
-        <Card style={{width: "140px"}}>
-          <Image
-            alt="NextUI hero Image with delay"
+        
+          <MyAvatar
+            isBordered
             radius="md"
-            //color="danger"
+            color="danger"
             src={props.imagen}
             style={EventoCardStyles}
           />
-        </Card>
-        {/* <Avatar
-          isBordered
-          radius="md"
-          color="danger"
-          src={props.imagen}
-          style={EventoCardStyles}
-        /> */}
+        
       </Link>
     </div>
   );
