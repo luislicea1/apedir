610fe109197ae1b161aa082e17088f5540668577
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 17:42:53 -0500

    melman

--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -1,24 +1,20 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import "./seccion.css";
-//import ComponenteLugar from "./ComponenteLugar";
+import ComponenteLugar from "./ComponenteLugar";
 import { grid_3_col } from "../styles/styles";
-
-const ComponenteLugar = lazy(() => import("./ComponenteLugar"));
 
 export default function ListadoDeComponentesLugar(props) {
   const lugares = props.lugares;
   return (
     <div className="list-container" style={grid_3_col}>
       {lugares.map((lugar, index) => (
-        <Suspense>
-          <ComponenteLugar
-            key={index}
-            nombre={lugar.nombre}
-            localizacion={lugar.localizacion}
-            numeroPersonas={lugar.numeroPersonas}
-            imagen={lugar.imagen}
-          />
-        </Suspense>
+        <ComponenteLugar
+          key={index}
+          nombre={lugar.nombre}
+          localizacion={lugar.localizacion}
+          numeroPersonas={lugar.numeroPersonas}
+          imagen={lugar.imagen}
+        />
       ))}
     </div>
   );
