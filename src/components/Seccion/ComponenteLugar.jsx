8e8e58ecafd3613dc88b115f19b280ca54b1b654
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 17:29:30 -0500

    mas lazy bla vla

--- src/components/Seccion/ComponenteLugar.jsx
+++ src/components/Seccion/ComponenteLugar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Card, CardHeader, CardBody, Link } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { CardStyles, ImgCardStyle, LogoStyle } from "../styles/styles";
-import Stars from "../Stars/Stars";
+//import Stars from "../Stars/Stars";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { Helmet } from "react-helmet";
+
+const Stars = lazy(() => import("../Stars/Stars"));
 
 export default function ComponenteLugar(props) {
   const navigate = useNavigate();
@@ -19,34 +21,49 @@ export default function ComponenteLugar(props) {
   };
 
   return (
-    <Link onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}>
+    <Link
+      onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}
+    >
       <Helmet>
-        <link fetchpriority="high" rel="preload" href={props.imagen} as="image" imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"/>
+        <link
+          fetchpriority="high"
+          rel="preload"
+          href={props.imagen}
+          as="image"
+          imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"
+        />
       </Helmet>
-      <Card className="py-4" style={CardStyles}>
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">{props.localizacion}</p>
-          <h2 className="font-bold text-large mb-2" style={{ fontSize: "25px" }}>
-            {props.nombre}
-          </h2>
-          <Stars readOnly w={100} rating={3.5}></Stars>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
-          <LazyLoadImage
-            alt={props.nombre}
-            src={props.imagen}
-            effect="blur"
-            style={{ ...LogoStyle, objectFit: "cover" }}
-            delayMethod="debounce"
-            delayTime={300}
-            placeholderSrc={props.imagen}
-            useIntersectionObserver={true}
-            visibleByDefault={true}
-          />
-          {/* Asegúrate de que props.imagen.caption esté definido antes de intentar mostrarlo */}
-          {props.imagen.caption && <span>{props.imagen.caption}</span>}
-        </CardBody>
-      </Card>
+      <Suspense>
+        <Card className="py-4" style={CardStyles}>
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold">
+              {props.localizacion}
+            </p>
+            <h2
+              className="font-bold text-large mb-2"
+              style={{ fontSize: "25px" }}
+            >
+              {props.nombre}
+            </h2>
+            <Stars readOnly w={100} rating={3.5}></Stars>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
+            <LazyLoadImage
+              alt={props.nombre}
+              src={props.imagen}
+              effect="blur"
+              style={{ ...LogoStyle, objectFit: "cover" }}
+              delayMethod="debounce"
+              delayTime={300}
+              placeholderSrc={props.imagen}
+              useIntersectionObserver={true}
+              visibleByDefault={true}
+            />
+            {/* Asegúrate de que props.imagen.caption esté definido antes de intentar mostrarlo */}
+            {props.imagen.caption && <span>{props.imagen.caption}</span>}
+          </CardBody>
+        </Card>
+      </Suspense>
     </Link>
   );
 }
--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./seccion.css";
-import ComponenteLugar from "./ComponenteLugar";
+//import ComponenteLugar from "./ComponenteLugar";
 import { grid_3_col } from "../styles/styles";
+
+const ComponenteLugar = lazy(() => import("./ComponenteLugar"));
 
 export default function ListadoDeComponentesLugar(props) {
   const lugares = props.lugares;
   return (
     <div className="list-container" style={grid_3_col}>
       {lugares.map((lugar, index) => (
-        <ComponenteLugar
-          key={index}
-          nombre={lugar.nombre}
-          localizacion={lugar.localizacion}
-          numeroPersonas={lugar.numeroPersonas}
-          imagen={lugar.imagen}
-        />
+        <Suspense>
+          <ComponenteLugar
+            key={index}
+            nombre={lugar.nombre}
+            localizacion={lugar.localizacion}
+            numeroPersonas={lugar.numeroPersonas}
+            imagen={lugar.imagen}
+          />
+        </Suspense>
       ))}
     </div>
   );
