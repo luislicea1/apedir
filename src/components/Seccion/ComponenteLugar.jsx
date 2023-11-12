e0f0992f564c006ff5093cbdffdd57c6ed215642
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-11 22:16:25 -0500

    sdsdsdsdsdfff

--- src/components/Seccion/ComponenteLugar.jsx
+++ src/components/Seccion/ComponenteLugar.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Card, CardHeader, CardBody, Link, Image } from "@nextui-org/react";
+import React from "react";
+import { Card, CardHeader, CardBody, Link } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { CardStyles, ImgCardStyle, LogoStyle } from "../styles/styles";
@@ -17,31 +17,28 @@ export default function ComponenteLugar(props) {
     numeroPersonas: PropTypes.number.isRequired,
     imagen: PropTypes.string.isRequired,
   };
-  
 
   return (
-    <Link
-      onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}
-    > 
-      <Helmet>
-       <link rel="preload" href={props.imagen} as="image" imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"/>
-     </Helmet>
-      
+    <Link onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}>
       <Card className="py-4" style={CardStyles}>
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
           <p className="text-tiny uppercase font-bold">{props.localizacion}</p>
-          <h2
-            className="font-bold text-large mb-2"
-            style={{ fontSize: "25px" }}
-          >
+          <h2 className="font-bold text-large mb-2" style={{ fontSize: "25px" }}>
             {props.nombre}
           </h2>
           <Stars readOnly w={100} rating={3.5}></Stars>
         </CardHeader>
         <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
-          
+          <Helmet>
+            <link
+              rel="preload"
+              href={props.imagen}
+              as="image"
+              imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"
+            />
+          </Helmet>
           <LazyLoadImage
-            alt="NextUI hero Image with delay"
+            alt={props.nombre}
             className="object-cover rounded-xl"
             src={props.imagen}
             effect="blur"
@@ -50,9 +47,10 @@ export default function ComponenteLugar(props) {
             delayTime={300}
             placeholderSrc={props.imagen}
             useIntersectionObserver={true}
-            visibleByDefault = {true}
+            visibleByDefault={true}
           />
-          <span>{props.imagen.caption}</span>
+          {/* Comentario: No está claro de dónde proviene props.imagen.caption, puedes ajustarlo según tus necesidades */}
+          {props.imagen.caption && <span>{props.imagen.caption}</span>}
         </CardBody>
       </Card>
     </Link>
