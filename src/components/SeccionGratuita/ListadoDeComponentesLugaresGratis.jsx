57024cefe5ad350876fc511b25fdf08f055c477e
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 18:11:27 -0500

    aaaaaaaa ziguena

--- src/components/SeccionGratuita/ListadoDeComponentesLugaresGratis.jsx
+++ src/components/SeccionGratuita/ListadoDeComponentesLugaresGratis.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 //import './seccion.css'
 import ComponenteLugarGratis from "./ComponenteLugarGratis";
 import { grid_3_col } from "../styles/styles";
 
+const ComponenteLugarGratis = lazy(() => import("./ComponenteLugarGratis"));
+
 export default function ListadoDeComponentesLugarGratis(props) {
-    
-  const lugares = props.lugares
+  const lugares = props.lugares;
   return (
     <div className="list-container" style={grid_3_col}>
-        {lugares.map((lugar, index) => (
-        <ComponenteLugarGratis
-          key={index}
-          nombre={lugar.nombre}
-          localizacion={lugar.localizacion}
-          numeroPersonas = {lugar.numeroPersonas}
-          imagen = {lugar.imagen}
-        />
+      {lugares.map((lugar, index) => (
+        <Suspense>
+          <ComponenteLugarGratis
+            key={index}
+            nombre={lugar.nombre}
+            localizacion={lugar.localizacion}
+            numeroPersonas={lugar.numeroPersonas}
+            imagen={lugar.imagen}
+          />
+        </Suspense>
       ))}
     </div>
   );
