6425d2d7d152b64d048a1f03d815db09fbbd8579
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 17:39:08 -0500

    kila

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,13 @@ import React ,{lazy, Suspense}from 'react'
 //import SeccionMR from "../components/SeccionMasRecomendados/SeccionMR";
 //import { lugaresRecomendados } from "../components/Lugares/LugaresRecomendados";
 import { lugares } from '../components/Lugares/Lugares';
-import SeccionEventos from "../components/Eventos/SeccionEventos";
-import { eventos } from "../components/Lugares/Eventos";
-import Seccion from '../components/Seccion/Seccion';
+//import SeccionEventos from "../components/Eventos/SeccionEventos";
+//import { eventos } from "../components/Lugares/Eventos";
+//import Seccion from '../components/Seccion/Seccion';
 import { Helmet } from 'react-helmet';
 
 const Header = lazy(()=>import ('../components/header/Header'))
-//const Seccion = lazy(()=> import('../components/Seccion/Seccion'));
+const Seccion = lazy(()=> import('../components/Seccion/Seccion'));
 const renderLoader = () => <p>Loading</p>;
 
 export default function Home() {
