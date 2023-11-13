c94a50b005f2907dc0677f9772e024ed6725098c
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 22:13:39 -0500

    asdqwejpasdopqwe

--- src/components/Seccion/ComponenteLugar.jsx
+++ src/components/Seccion/ComponenteLugar.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import { Card, CardHeader, CardBody, Link } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -7,21 +7,10 @@ import Stars from "../Stars/Stars";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { Helmet } from "react-helmet";
-import { useInView } from 'react-intersection-observer';
+
 
 export default function ComponenteLugar(props) {
   const navigate = useNavigate();
-  const { ref, inView } = useInView({
-    /* Opciones */
-    threshold: 0,
-  });
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  React.useEffect(() => {
-    if (inView && !isLoaded) {
-      setIsLoaded(true);
-    }
-  }, [inView, isLoaded]);
 
   ComponenteLugar.propTypes = {
     localizacion: PropTypes.string.isRequired,
@@ -30,55 +19,54 @@ export default function ComponenteLugar(props) {
     imagen: PropTypes.string.isRequired,
   };
 
-  return (
-    <div ref={ref}>
-      {isLoaded && (
-        <Suspense fallback={<div>Cargando...</div>}>
-          <Link
-            onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}
-          >
-            <Helmet>
-              <link
-                fetchpriority="high"
-                rel="preload"
-                href={props.imagen}
-                as="image"
-                imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"
-              />
-            </Helmet>
+  
 
-            <Card className="py-4" style={CardStyles}>
-              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold">
-                  {props.localizacion}
-                </p>
-                <h2
-                  className="font-bold text-large mb-2"
-                  style={{ fontSize: "25px" }}
-                >
-                  {props.nombre}
-                </h2>
-                <Stars readOnly w={100} rating={3.5}></Stars>
-              </CardHeader>
-              <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
-                <LazyLoadImage
-                  alt={props.nombre}
-                  src={props.imagen}
-                  effect="blur"
-                  style={{ ...LogoStyle, objectFit: "cover" }}
-                  delayMethod="debounce"
-                  delayTime={300}
-                  placeholderSrc={props.imagen}
-                  useIntersectionObserver={true}
-                  visibleByDefault={true}
-                />
-                {/* Asegúrate de que props.imagen.caption esté definido antes de intentar mostrarlo */}
-                {props.imagen.caption && <span>{props.imagen.caption}</span>}
-              </CardBody>
-            </Card>
-          </Link>
-        </Suspense>
-      )}
-    </div>
+  return (
+    <Suspense>
+      <Link
+        onClick={() => navigate(`/lugar/${props.localizacion}/${props.nombre}`)}
+      >
+        <Helmet>
+          <link
+            fetchpriority="high"
+            rel="preload"
+            href={props.imagen}
+            as="image"
+            imagesrcset="image_400px.jpg 400w, image_800px.jpg 800w"
+          />
+        </Helmet>
+
+        <Card className="py-4" style={CardStyles}>
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            
+            <p className="text-tiny uppercase font-bold">
+              {props.localizacion}
+            </p>
+            <h2
+              className="font-bold text-large mb-2"
+              style={{ fontSize: "25px" }}
+            >
+              {props.nombre}
+            </h2>
+            <Stars readOnly w={100} rating={3.5}></Stars>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2" style={ImgCardStyle}>
+            <LazyLoadImage
+              alt={props.nombre}
+              src={props.imagen}
+              effect="blur"
+              style={{ ...LogoStyle, objectFit: "cover" }}
+              delayMethod="debounce"
+              delayTime={300}
+              placeholderSrc={props.imagen}
+              useIntersectionObserver={true}
+              visibleByDefault={true}
+            />
+            {/* Asegúrate de que props.imagen.caption esté definido antes de intentar mostrarlo */}
+            {props.imagen.caption && <span>{props.imagen.caption}</span>}
+          </CardBody>
+        </Card>
+      </Link>
+    </Suspense>
   );
 }
