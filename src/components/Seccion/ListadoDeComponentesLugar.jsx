fe76b5ff4244a6841a04e0f2b4530ffb6b93ac61
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 21:47:23 -0500

    aydgyqbjdoqndksndasd

--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -1,41 +1,44 @@
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import "./seccion.css";
-import ComponenteLugar from "./ComponenteLugar";
 import { grid_3_col } from "../styles/styles";
 import { useInView } from 'react-intersection-observer';
+
+const ComponenteLugar = React.lazy(() => import("./ComponenteLugar"));
 
 export default function ListadoDeComponentesLugar(props) {
  const lugares = props.lugares;
 
  return (
-   <div className="list-container" style={grid_3_col}>
-     {lugares.map((lugar, index) => {
-       const { ref, inView } = useInView({
-         /* Opciones */
-         threshold: 0,
-       });
+  <div className="list-container" style={grid_3_col}>
+    {lugares.map((lugar, index) => {
+      const { ref, inView } = useInView({
+        /* Opciones */
+        threshold: 0,
+      });
 
-       const [isVisible, setIsVisible] = useState(false);
+      const [isLoaded, setIsLoaded] = useState(false);
 
-       React.useEffect(() => {
-         if (inView) {
-           setIsVisible(true);
-         }
-       }, [inView]);
+      React.useEffect(() => {
+        if (inView && !isLoaded) {
+          setIsLoaded(true);
+        }
+      }, [inView, isLoaded]);
 
-       return (
-         <div ref={ref} key={index}>
-           {isVisible && (
-             <ComponenteLugar
+      return (
+        <div ref={ref} key={index}>
+          {isLoaded && (
+            <Suspense fallback={<div>Cargando...</div>}>
+              <ComponenteLugar
                nombre={lugar.nombre}
                localizacion={lugar.localizacion}
                numeroPersonas={lugar.numeroPersonas}
                imagen={lugar.imagen}
-             />
-           )}
-         </div>
-       );
-     })}
-   </div>
+              />
+            </Suspense>
+          )}
+        </div>
+      );
+    })}
+  </div>
  );
 }
