f0d48b1ccc7e2b72de09d66eeadd5238c2827031
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-12 21:50:24 -0500

    regreso

--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -1,44 +1,21 @@
-import React, { Suspense, useState } from "react";
+import React from "react";
 import "./seccion.css";
+import ComponenteLugar from "./ComponenteLugar";
 import { grid_3_col } from "../styles/styles";
-import { useInView } from 'react-intersection-observer';
-
-const ComponenteLugar = React.lazy(() => import("./ComponenteLugar"));
 
 export default function ListadoDeComponentesLugar(props) {
- const lugares = props.lugares;
-
- return (
-  <div className="list-container" style={grid_3_col}>
-    {lugares.map((lugar, index) => {
-      const { ref, inView } = useInView({
-        /* Opciones */
-        threshold: 0,
-      });
-
-      const [isLoaded, setIsLoaded] = useState(false);
-
-      React.useEffect(() => {
-        if (inView && !isLoaded) {
-          setIsLoaded(true);
-        }
-      }, [inView, isLoaded]);
-
-      return (
-        <div ref={ref} key={index}>
-          {isLoaded && (
-            <Suspense fallback={<div>Cargando...</div>}>
-              <ComponenteLugar
-               nombre={lugar.nombre}
-               localizacion={lugar.localizacion}
-               numeroPersonas={lugar.numeroPersonas}
-               imagen={lugar.imagen}
-              />
-            </Suspense>
-          )}
-        </div>
-      );
-    })}
-  </div>
- );
+  const lugares = props.lugares;
+  return (
+    <div className="list-container" style={grid_3_col}>
+      {lugares.map((lugar, index) => (
+        <ComponenteLugar
+          key={index}
+          nombre={lugar.nombre}
+          localizacion={lugar.localizacion}
+          numeroPersonas={lugar.numeroPersonas}
+          imagen={lugar.imagen}
+        />
+      ))}
+    </div>
+  );
 }
