41a8697f584c7e7310d6979372fde777ed9cd928
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-13 00:21:58 -0500

    memo

--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -26,59 +26,56 @@ import { grid_3_col } from "../styles/styles";
 import "./seccion.css";
 
 // Importar ComponenteLugar de manera diferida
-const ComponenteLugar = lazy(()=>import ("./ComponenteLugar"))
+const ComponenteLugar = React.memo(lazy(() => import("./ComponenteLugar")));
 
 const ListadoDeComponentesLugar = ({ lugares }) => {
- const [items, setItems] = useState([]);
- const [hasMore, setHasMore] = useState(true);
- const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
- useEffect(() => {
- const initialItems = lugares.slice(0, 6);
- setItems(initialItems);
- if (lugares.length <= 6) {
-   setHasMore(false);
- }
- }, [lugares]);
+  useEffect(() => {
+    const initialItems = lugares.slice(0, 6);
+    setItems(initialItems);
+    if (lugares.length <= 6) {
+      setHasMore(false);
+    }
+  }, [lugares]);
 
- const fetchMoreData = () => {
- setIsLoading(true);
- setTimeout(() => {
-   const nextItems = lugares.slice(items.length, items.length + 3);
-   setItems([...items, ...nextItems]);
-   setIsLoading(false);
-   if (items.length + nextItems.length >= lugares.length) {
-     setHasMore(false);
-   }
- }, 1500);
- };
+  const fetchMoreData = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const nextItems = lugares.slice(items.length, items.length + 3);
+      setItems([...items, ...nextItems]);
+      setIsLoading(false);
+      if (items.length + nextItems.length >= lugares.length) {
+        setHasMore(false);
+      }
+    }, 1500);
+   };
+   
 
- return (
- <InfiniteScroll
-   dataLength={items.length}
-   next={fetchMoreData}
-   hasMore={hasMore}
-   loader={isLoading ? <h4>Loading...</h4> : null}
- >
-   <div
-     className="list-container" style={grid_3_col}
-   >
-     {items.map((lugar, index) => (
-       <Suspense fallback={<div>Loading...</div>}>
-         <ComponenteLugar
-           key={index}
-           nombre={lugar.nombre}
-           localizacion={lugar.localizacion}
-           numeroPersonas={lugar.numeroPersonas}
-           imagen={lugar.imagen}
-         />
-       </Suspense>
-     ))}
-   </div>
- </InfiniteScroll>
- );
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={fetchMoreData}
+      hasMore={hasMore}
+      loader={isLoading ? <h4>Loading...</h4> : null}
+    >
+      <div className="list-container" style={grid_3_col}>
+        {items.map((lugar, index) => (
+          <Suspense fallback={<div>Loading...</div>}>
+            <ComponenteLugar
+              key={index}
+              nombre={lugar.nombre}
+              localizacion={lugar.localizacion}
+              numeroPersonas={lugar.numeroPersonas}
+              imagen={lugar.imagen}
+            />
+          </Suspense>
+        ))}
+      </div>
+    </InfiniteScroll>
+  );
 };
 
 export default ListadoDeComponentesLugar;
-
-
