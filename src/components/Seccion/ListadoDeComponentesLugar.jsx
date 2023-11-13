7b18c452aa4116237889f2f358202007a84018e7
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-13 00:14:19 -0500

    aooaowoaoao

--- src/components/Seccion/ListadoDeComponentesLugar.jsx
+++ src/components/Seccion/ListadoDeComponentesLugar.jsx
@@ -20,65 +20,63 @@
 //   );
 // }
 
-
-import React, { useState, useEffect,lazy } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-//import ComponenteLugar from "./ComponenteLugar";
 import { grid_3_col } from "../styles/styles";
 import "./seccion.css";
 
+// Importar ComponenteLugar de manera diferida
 const ComponenteLugar = lazy(()=>import ("./ComponenteLugar"))
 
 const ListadoDeComponentesLugar = ({ lugares }) => {
-  const [items, setItems] = useState([]);
-  const [hasMore, setHasMore] = useState(true);
+ const [items, setItems] = useState([]);
+ const [hasMore, setHasMore] = useState(true);
+ const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    // Cargar los primeros 6 elementos al inicio
-    const initialItems = lugares.slice(0, 6);
-    setItems(initialItems);
+ useEffect(() => {
+ const initialItems = lugares.slice(0, 6);
+ setItems(initialItems);
+ if (lugares.length <= 6) {
+   setHasMore(false);
+ }
+ }, [lugares]);
 
-    // Desactivar la carga si no hay más elementos
-    if (lugares.length <= 6) {
-      setHasMore(false);
-    }
-  }, [lugares]);
+ const fetchMoreData = () => {
+ setIsLoading(true);
+ setTimeout(() => {
+   const nextItems = lugares.slice(items.length, items.length + 3);
+   setItems([...items, ...nextItems]);
+   setIsLoading(false);
+   if (items.length + nextItems.length >= lugares.length) {
+     setHasMore(false);
+   }
+ }, 1500);
+ };
 
-  const fetchMoreData = () => {
-    // Simular la carga de más elementos después de 6
-    setTimeout(() => {
-      const nextItems = lugares.slice(items.length, items.length + 3);
-      setItems([...items, ...nextItems]);
-
-      // Desactivar la carga si no hay más elementos
-      if (items.length + nextItems.length >= lugares.length) {
-        setHasMore(false);
-      }
-    }, 1500);
-  };
-
-  return (
-    <InfiniteScroll
-      dataLength={items.length}
-      next={fetchMoreData}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-    >
-      <div
-        className="list-container" style={grid_3_col}
-      >
-        {items.map((lugar, index) => (
-          <ComponenteLugar
-            key={index}
-            nombre={lugar.nombre}
-            localizacion={lugar.localizacion}
-            numeroPersonas={lugar.numeroPersonas}
-            imagen={lugar.imagen}
-          />
-        ))}
-      </div>
-    </InfiniteScroll>
-  );
+ return (
+ <InfiniteScroll
+   dataLength={items.length}
+   next={fetchMoreData}
+   hasMore={hasMore}
+   loader={isLoading ? <h4>Loading...</h4> : null}
+ >
+   <div
+     className="list-container" style={grid_3_col}
+   >
+     {items.map((lugar, index) => (
+       <Suspense fallback={<div>Loading...</div>}>
+         <ComponenteLugar
+           key={index}
+           nombre={lugar.nombre}
+           localizacion={lugar.localizacion}
+           numeroPersonas={lugar.numeroPersonas}
+           imagen={lugar.imagen}
+         />
+       </Suspense>
+     ))}
+   </div>
+ </InfiniteScroll>
+ );
 };
 
 export default ListadoDeComponentesLugar;
