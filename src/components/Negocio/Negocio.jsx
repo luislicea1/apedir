d9b04276332749af283268d5ef8f4be9bdfbd76f
ClaudiaQueipo <claudia.queipog@gmail.com>
2023-11-18 09:01:33 -0500

    MOD: optimización de la parte de mostrar los negocios

--- src/components/Negocio/Negocio.jsx
+++ src/components/Negocio/Negocio.jsx
@@ -1,158 +1,130 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
+import React, { useState, useEffect, lazy, Suspense, useCallback, memo } from "react";
 
-import PropTypes from "prop-types";
-import Imagen from "../../assets/fondo/restaurant.webp";
-import LogoImg from "../../assets/img/img (1).webp";
 import Header from "../header/Header";
-//import PortadaDeNegocio from "./PortadaDeNegocio/portadaNegocio";
-//import HeaderNegocio from "./HeaderNegocio/HeaderNegocio";
-//import DescripcionNegocio from "./Descripcion/Descripcion";
-//import TituloNegocio from "./TituloNegocio/TituloNegocio";
-//import ListadoProductos from "./Productos/ListadoProductos";
-//import Promo from "./Promo/Promo";
-import { comidas } from "../Lugares/Comidas";
-import { desayuno } from "../Lugares/Comidas";
-//import FooterNegocio from "./Footer/FooterNegocio";
 import { NegocioSection } from "../styles/styles";
 import Navegacion from "./HeaderNegocio/Navegacion";
-//import Stars from "../Stars/Stars";
-import { Helmet } from "react-helmet";
 import { fetchBussinessPerURL } from "../../api/bussiness";
 import { getCategories } from "../../api/categories";
 import { getProducts } from "../../api/products";
 
 const text =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis quam laboriosam eveniet voluptatibus iste esse, consectetur iure distinctio, iusto reprehenderit vel! Recusandae distinctio laboriosam optio, quam at vero iure! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis quam laboriosam eveniet voluptatibus iste esse, consectetur iure distinctio, iusto repr";
+ "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis quam laboriosam eveniet voluptatibus iste esse, consectetur iure distinctio, iusto reprehenderit vel! Recusandae distinctio laboriosam optio, quam at vero iure! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis quam laboriosam eveniet voluptatibus iste esse, consectetur iure distinctio, iusto repr";
 
 const Stars = lazy(() => import("../Stars/Stars"));
-//const Navegacion = lazy(() => import("./HeaderNegocio/Navegacion"));
 const FooterNegocio = lazy(() => import("./Footer/FooterNegocio"));
 const Promo = lazy(() => import("./Promo/Promo"));
 const ListadoProductos = lazy(() => import("./Productos/ListadoProductos"));
 const TituloNegocio = lazy(() => import("./TituloNegocio/TituloNegocio"));
 const DescripcionNegocio = lazy(() => import("./Descripcion/Descripcion"));
 const PortadaDeNegocio = lazy(() =>
-  import("./PortadaDeNegocio/portadaNegocio")
+ import("./PortadaDeNegocio/portadaNegocio")
 );
 
 const renderLoader = () => <p>Loading</p>;
 
-export default function Negocio({ url }) {
-  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+export default memo(function Negocio({ url }) {
+ const [isNavbarVisible, setIsNavbarVisible] = useState(false);
 
-  const [bussiness, setBussiness] = useState(null);
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const bussinessData = await fetchBussinessPerURL(url);
-      setBussiness(bussinessData);
-    };
+ const [bussiness, setBussiness] = useState(null);
+ const [categories, setCategories] = useState([]);
+ const [products, setProducts] = useState([]);
 
-    fetchData();
-  }, [url]);
+ const fetchData = useCallback(async () => {
+   const bussinessData = await fetchBussinessPerURL(url);
+   setBussiness(bussinessData);
+ }, [url]);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const categoryList = await getCategories(bussiness.id);
-      setCategories(categoryList);
-    };
-    console.log(bussiness);
-    if (bussiness !== null && bussiness !== undefined && bussiness?.id)
-      fetchCategories();
-    if (categories.length > 0) setIsNavbarVisible(true);
-  }, [bussiness]);
+ useEffect(() => {
+   fetchData();
+ }, [url]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const productList = await getProducts(categories);
-      setProducts(productList !== null ? productList : []);
-    };
-    fetchProducts();
-  }, [categories]);
+ useEffect(() => {
+   const fetchCategories = async () => {
+     const categoryList = await getCategories(bussiness.id);
+     setCategories(categoryList);
+   };
+   if (bussiness !== null && bussiness !== undefined && bussiness?.id)
+     fetchCategories();
+   if (categories.length > 0) setIsNavbarVisible(true);
+ }, [bussiness]);
 
-  useEffect(() => {
-    const checkScrollPosition = () => {
-      const firstProductListPosition = document
-        .querySelector(".first-product-list")
-        .getBoundingClientRect().top;
-      if (window.pageYOffset >= firstProductListPosition) {
-        setIsNavbarVisible(true);
-      } else {
-        setIsNavbarVisible(false);
-      }
-    };
+ useEffect(() => {
+   const fetchProducts = async () => {
+     const productList = await getProducts(categories);
+     setProducts(productList !== null ? productList : []);
+   };
+   if (categories.length > 0) fetchProducts();
+ }, [categories]);
 
-    window.addEventListener("scroll", checkScrollPosition);
+ useEffect(() => {
+   const checkScrollPosition = () => {
+     const firstProductListPosition = document
+       .querySelector(".first-product-list")
+       .getBoundingClientRect().top;
+     if (window.pageYOffset >= firstProductListPosition) {
+       setIsNavbarVisible(true);
+     } else {
+       setIsNavbarVisible(false);
+     }
+   };
 
-    return () => {
-      window.removeEventListener("scroll", checkScrollPosition);
-    };
-  }, []);
+   window.addEventListener("scroll", checkScrollPosition);
 
-  return bussiness ? (
-    <div className="container flex z-40 w-full h-auto items-center justify-center data-[menu-open=true]:border-none  top-0 inset-x-0   backdrop-blur-lg data-[menu-open=true]:backdrop-blur-xl backdrop-saturate-150 bg-background/70">
-      {/* <Helmet>
-        <meta
-          name="description"
-          content="pagina de promocion de negocios en cuba"
-        />
-        <title>{nombre}</title>
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href={LogoImg}
-          alt="logo apedir"
-        />dame 
-      </Helmet> */}
-      <section style={NegocioSection}>
-        <Header
-          logo={bussiness.perfil_pic}
-          nombre={bussiness.name}
-          horario={"si"}
-          anterior={"/"}
-        />
-        {isNavbarVisible && <Navegacion links={categories} />}
-        <section className="section" style={NegocioSection}>
-          <PortadaDeNegocio
-            imagenPortada={bussiness.front_pic}
-          ></PortadaDeNegocio>
-          <div className="p-2 m-2">
-            <Suspense fallback={renderLoader()}>
-              <TituloNegocio title={bussiness.name}></TituloNegocio>
-              <Stars w={100}></Stars>
-              <DescripcionNegocio
-                descripcion={bussiness.description}
-                contact={"si"}
-                domicilio={"si"}
-                localizacion={bussiness.address}
-                gps_location={bussiness.gps_location}
-                like={"si"}
-              ></DescripcionNegocio>
+   return () => {
+     window.removeEventListener("scroll", checkScrollPosition);
+   };
+ }, []);
 
-              <Promo seguidores={300} productos={200} lesGusta={1200}></Promo>
-            </Suspense>
+ return bussiness ? (
+   <div className="container flex z-40 w-full h-auto items-center justify-center data-[menu-open=true]:border-none top-0 inset-x-0  backdrop-blur-lg data-[menu-open=true]:backdrop-blur-xl backdrop-saturate-150 bg-background/70">
+     <section style={NegocioSection}>
+       <Header
+         logo={bussiness.perfil_pic}
+         nombre={bussiness.name}
+         horario={"si"}
+         anterior={"/"}
+       />
+       {isNavbarVisible && <Navegacion links={categories} />}
+       <section className="section" style={NegocioSection}>
+         <PortadaDeNegocio
+           imagenPortada={bussiness.front_pic}
+         ></PortadaDeNegocio>
+         <div className="p-2 m-2">
+           <Suspense fallback={renderLoader()}>
+             <TituloNegocio title={bussiness.name}></TituloNegocio>
+             <Stars w={100}></Stars>
+             <DescripcionNegocio
+               descripcion={bussiness.description}
+               contact={"si"}
+               domicilio={"si"}
+               localizacion={bussiness.address}
+               gps_location={bussiness.gps_location}
+               like={"si"}
+             ></DescripcionNegocio>
 
-            <div className="first-product-list"></div>
-            <Suspense fallback={renderLoader()}>
-              {categories.map((category, idx) => {
-                const categoryProducts = products.filter(
-                  (product) => product.category === category.id
-                );
-                return (
-                  <ListadoProductos
-                    id={idx}
-                    key={idx}
-                    title={category.category}
-                    nombre={category.category}
-                    localizacion={category.category}
-                    lista={categoryProducts}
-                  ></ListadoProductos>
-                );
-              })}
-            </Suspense>
-          </div>
-        </section>
+             <Promo seguidores={300} productos={200} lesGusta={1200}></Promo>
+           </Suspense>
+
+           <div className="first-product-list"></div>
+           <Suspense fallback={renderLoader()}>
+             {categories.map((category, idx) => {
+               const categoryProducts = products.filter(
+                (product) => product.category === category.id
+               );
+               return (
+                <ListadoProductos
+                  id={idx}
+                  key={idx}
+                  title={category.category}
+                  nombre={category.category}
+                  localizacion={category.category}
+                  lista={categoryProducts}
+                ></ListadoProductos>
+               );
+             })}
+           </Suspense>
+         </div>
+       </section>
         <Suspense fallback={renderLoader()}>
           <FooterNegocio
             title={"nombre"}
@@ -165,9 +137,4 @@ export default function Negocio({ url }) {
   ) : (
     <>loading</>
   );
-}
-
-Negocio.propTypes = {
-  //nombre: PropTypes.string.isRequired,
-  //localizacion: PropTypes.string.isRequired,
-};
+})
