89a051d06be0cfa40970c3b57d18a85d48c66eca
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-20 21:11:50 -0500

    wpoooskdsmdksmd

--- src/components/Seccion/Seccion.jsx
+++ src/components/Seccion/Seccion.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import TituloDeSeccion from "./TituloDeSeccion";
 import { container, section } from "../styles/styles";
 //import { CircularProgress } from "@nextui-org/react";
-import ListadoDeComponentesLugar from "./ListadoDeComponentesLugar";
-//import Prueba3 from "../prueba/Prueba3";
+//import ListadoDeComponentesLugar from "./ListadoDeComponentesLugar";
+import Prueba3 from "../prueba/Prueba3";
 //import BusinessPage from "../prueba/Prueba4";
 
 // import { loadMoreBussiness } from "../../api/bussiness";
@@ -16,9 +16,10 @@ export default function Seccion(props) {
     >
       <section className="section px-6" style={section}>
         <TituloDeSeccion title={props.title}></TituloDeSeccion>
-        <ListadoDeComponentesLugar /> 
+        {/* <ListadoDeComponentesLugar />   */}
        
-        {/* <Prueba3></Prueba3> */}
+         <Prueba3></Prueba3> 
+        {/* <BusinessPage/> */}
       </section>
     </div>
   );
