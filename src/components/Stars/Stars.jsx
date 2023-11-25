b72e246d628bc2164841f9e0bb1959765ab7e229
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-24 20:13:46 -0500

    Merge branch 'main' of github.com:luislicea1/apedir into main

--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -4,14 +4,16 @@ import { Rating, RoundedStar } from "@smastrom/react-rating";
 import { addStars } from "../../api/starsRate";
 
 export default function Stars(props) {
+
   const handleChangeStars = async (rate) => {
-    console.log(rate);
+    
     try {
       await addStars(rate, props.user, props.bussiness);
     } catch (error) {
       console.error("Error adding stars:", error);
     }
   };
+
 
   const itemStyles = {
     itemShapes: RoundedStar,
@@ -24,11 +26,11 @@ export default function Stars(props) {
   return (
     <Rating
       style={{ maxWidth: width }}
-      value={props.rating}
+      value={props.rate}
       onChange={(event) => {
         const newRating = event;
         handleChangeStars(newRating).then(() => {
-          props.setRating(newRating);
+          props.setRate(newRating);
         });
       }}
       {...(props.readOnly ? { readOnly: true } : {})}
