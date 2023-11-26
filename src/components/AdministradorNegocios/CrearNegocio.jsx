676f4cf8b04f0210f0d6bc2e61648f92ee580346
ClaudiaQueipo <claudia.queipog@gmail.com>
2023-11-25 21:51:06 -0500

    TEST: Prueba de responsive en crear negocio #1

--- src/components/AdministradorNegocios/CrearNegocio.jsx
+++ src/components/AdministradorNegocios/CrearNegocio.jsx
@@ -17,9 +17,9 @@ export default function CrearNegocio({ children }) {
     gridTemplateColumns: "repeat(1,1fr)",
   };
 
-  const linkColor = {
-    margin: "10px 20px",
-  };
+  // const linkColor = {
+  //   margin: "10px 20px",
+  // };
 
   const channels = supabase
     .channel("custom-all-channel")
@@ -39,31 +39,32 @@ export default function CrearNegocio({ children }) {
     .subscribe();
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
       <div style={sectionStyle}>
         <div className="flex flex-col">
-          {/* <br /> */}
           <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              margin: "0px 20px",
-            }}
+            
+            // style={{
+            //   display: "flex",
+            //   justifyContent: "space-between",
+            // }}
           >
-            <Link to="/administrador-negocio/perfil" style={linkColor}>
-              Perfil
-            </Link>
-            {bussiness !== null && bussiness !== undefined && (
-              <section style={{ display: "flex" }}>
-                <Link to="/administrador-negocio/horarios" style={linkColor}>
-                  Horario
-                </Link>
-                <Link to="/administrador-negocio/eventos" style={linkColor}>
-                  Eventos
-                </Link>
-                <Link to="/administrador-negocio/productos" style={linkColor}>
-                  Productos
-                </Link>
+            {bussiness !== null && bussiness !== undefined ? (
+              <section className="flex justify-between" style={{margin: "10px"}}
+              >
+                <Link to="/administrador-negocio/perfil">Perfil</Link>
+
+                <Link to="/administrador-negocio/horarios">Horario</Link>
+                <Link to="/administrador-negocio/eventos">Eventos</Link>
+                <Link to="/administrador-negocio/productos">Productos</Link>
+              </section>
+            ) : (
+              <section
+              className="flex justify-between mx-8"
+              >
+                <Link to="/administrador-negocio/horarios">Horario</Link>
+                <Link to="/administrador-negocio/eventos">Eventos</Link>
+                <Link to="/administrador-negocio/productos">Productos</Link>
               </section>
             )}
           </div>
