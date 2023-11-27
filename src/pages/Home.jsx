aae23949fc03f0ab9f132e66c420779d5bbbf628
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-26 19:55:03 -0500

    nnnnnbbbbb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { lugares } from "../components/Lugares/Lugares";
 import { eventos } from "../components/Lugares/Eventos";
 import Seccion from "../components/Seccion/Seccion";
 import SeccionEventos from "../components/Eventos/SeccionEventos";
+import React from "react";
 
 export default function Home() {
   return (
