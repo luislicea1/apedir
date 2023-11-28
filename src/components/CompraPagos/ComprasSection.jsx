25e9082b1d9cc08685df8c3f3f84975a232baae4
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-28 01:09:45 -0500

    en el carrito aun

--- src/components/CompraPagos/ComprasSection.jsx
+++ src/components/CompraPagos/ComprasSection.jsx
@@ -1,35 +1,40 @@
 import React,{lazy, Suspense} from 'react'
-import Imagen2 from "../../assets/comidas/comida (3).webp";
-import Imagen3 from "../../assets/comidas/comida (4).webp";
 //import ListadoCompras from "./ListadoCompras";
 //import InputsPedido from "./InputsPedido";
 import { grid_1_col } from "../styles/styles";
+import { useCartStore } from '../../hooks/useStore';
 
 const ListadoCompras = lazy(()=>import ("./ListadoCompras"))
 const InputsPedido = lazy(()=>import ("./InputsPedido"))
 const renderLoader = () => <p>Loading</p>;
 
-const carrito = [
-  {
-    title: "Brochetas",
-    img: Imagen3,
-    cantidad: 1,
-    price: 5,
-  },
-  {
-    title: "Carne",
-    img: Imagen2,
-    cantidad: 3,
-    price: 10,
-  },
-];
+
 export default function ComprasSection() {
+  const carrito = useCartStore((state) => state.cart);
+  const setCarrito = useCartStore((state) => state.setCart);
+ 
+  console.log(carrito)
   
   return (
     
     <div style={grid_1_col}>
       <Suspense fallback={renderLoader()}>
-        <ListadoCompras lista={carrito}></ListadoCompras>
+        {/* <ListadoCompras lista={carrito}></ListadoCompras> */}
+        <div>Compras</div>
+        {carrito.map((product, index) => (
+             <div key={index}>
+              
+               <div style={{}}>
+                <img src={product.image} alt={product.title} width="50px" />
+                <span><p style={{maxWidth: "10ch", overflow: "hidden", textOverflow: "ellipsis"}}>{product.title}</p></span>
+                <span>{product.quantity}</span>
+                <span>$ {product.price}</span>
+               </div>
+               
+             
+             </div>
+           ))}
+
         <InputsPedido></InputsPedido>
       </Suspense>
     </div>
--- src/components/header/CarritoIcon.jsx
+++ src/components/header/CarritoIcon.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Badge } from "@nextui-org/react";
-//import { CartIcon } from "../Icons/CartIcon/CartIcon";
-import { grid_3_col, btn100pc } from "../styles/styles";
+import './header.css'
+
+import { grid_4_col, btn100pc } from "../styles/styles";
 import LogoCarritoNegro from "../../assets/logoReduce/LogoCarritoNegro";
 import {
   Dropdown,
@@ -11,53 +12,167 @@ import {
   Button,
 } from "@nextui-org/react";
 import { useCartStore } from "../../hooks/useStore";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 
-// En el componente Carrito
+/*
 export default function Carrito() {
   const [isInvisible, setIsInvisible] = React.useState(false);
   const carrito = useCartStore((state) => state.cart);
   const setCarrito = useCartStore((state) => state.setCart);
+  const [totalPrice, setTotalPrice] = React.useState(0);
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
+
 
   useEffect(() => {
-    console.log(carrito);
-  }, [carrito]);
+    const newTotalPrice = carrito.reduce((total, product) => total + product.price, 0);
+    setTotalPrice(newTotalPrice);
+   }, [carrito]);
+   
 
   
+ const isCarritoVacio = carrito.length === 0;
+
   return (
-    <div className="mt-2">
-      <Dropdown aria-label="carrito">
-        <DropdownTrigger aria-label="carrito">
-          <div>
-            <Badge
-              color="danger"
-              content={carrito.length}
-              isInvisible={isInvisible}
-              shape="circle"
-            >
-              <LogoCarritoNegro w={30} />
-            </Badge>
-          </div>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="carrito" className="no-hover">
-          {carrito.map((product, index) => (
-            <DropdownItem key={index}>
-              <div style={grid_3_col}>
+   <div className="mt-2">
+     {isCarritoVacio ? (         
+         <LogoCarritoNegro w={30} />
+
+     ) : (
+       <Dropdown aria-label="carrito">
+         <DropdownTrigger aria-label="carrito">
+           <div>
+             <Badge
+               color="danger"
+               content={carrito.length}
+               isInvisible={isInvisible}
+               shape="circle"
+             >
+               <LogoCarritoNegro w={30} />
+             </Badge>
+           </div>
+         </DropdownTrigger>
+         <DropdownMenu aria-label="carrito" className="no-hover">
+           {carrito.map((product, index) => (
+             <DropdownItem key={index}>
+              
+               <div style={grid_4_col}>
                 <img src={product.image} alt={product.title} width="50px" />
-                <span>{product.title}</span>
+                <span><p style={{maxWidth: "10ch", overflow: "hidden", textOverflow: "ellipsis"}}>{product.title}</p></span>
                 <span>{product.quantity}</span>
                 <span>$ {product.price}</span>
-              </div>
-            </DropdownItem>
-          ))}
-          <DropdownItem aria-label="carrito">
-            <a href="/carrito">
-              <Button color="primary" style={btn100pc}>
+               </div>
+               
+             
+             </DropdownItem>
+           ))}
+
+           <DropdownItem aria-label="carrito">
+            
+            <div style={{width: "100%", display: "flex", justifyContent: "center", marginTop: "10px"}}>
+              <p>Precio total: </p>
+              <strong>$ {totalPrice}</strong>
+            </div>
+           
+
+             <a href="/carrito">
+               <Button color="primary" style={btn100pc}>
                 Comprar
-              </Button>
-            </a>
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+               </Button>
+             </a> 
+            
+
+             
+           </DropdownItem>
+         </DropdownMenu>
+       </Dropdown>
+     )}
+   </div>
+ );
+}*/
+
+export default function Carrito() {
+  const [isInvisible, setIsInvisible] = React.useState(false);
+  const carrito = useCartStore((state) => state.cart);
+  const setCarrito = useCartStore((state) => state.setCart);
+  const [totalPrice, setTotalPrice] = React.useState(0);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  useEffect(() => {
+    const newTotalPrice = carrito.reduce(
+      (total, product) => total + product.price,
+      0
+    );
+    setTotalPrice(newTotalPrice);
+  }, [carrito]);
+
+  const isCarritoVacio = carrito.length === 0;
+
+  return (
+    <div className="mt-2">
+      {isCarritoVacio ? (
+        <LogoCarritoNegro w={30} />
+      ) : (
+        <>
+          <Button onPress={onOpen} className="carritoBtn2">
+          <Badge
+               color="danger"
+               content={carrito.length}
+               isInvisible={isInvisible}
+               shape="circle"
+             >
+               <LogoCarritoNegro w={30} />
+             </Badge>
+          </Button>
+
+          <Modal isOpen={isOpen} onOpenChange={onOpenChange} >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    Carrito
+                  </ModalHeader>
+                  <ModalBody>
+                    {carrito.map((product, index) => (
+                      <div key={index}>
+                        <div style={grid_4_col}>
+                          <img
+                            src={product.image}
+                            alt={product.title}
+                            width="50px"
+                          />
+                          <span>
+                            <p>
+                              {product.title}
+                            </p>
+                          </span>
+                          <span>{product.quantity}</span>
+                          <span>$ {product.price}</span>
+                        </div>
+                      </div>
+                    ))}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+                    <Button color="primary" onPress={onClose}>
+                      Action
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
