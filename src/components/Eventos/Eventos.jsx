9d8a0500e6d9ec7312b90c0632e515bdf89b6ec2
luislicea1 <luis.licea.berenguer@gmail.com>
2023-11-29 15:13:25 -0500

    eventos con carga diferida

--- src/components/Eventos/Eventos.jsx
+++ src/components/Eventos/Eventos.jsx
@@ -4,18 +4,33 @@ import { Avatar } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 //import MyAvatar from "./Avatar";
 import { EventoCardStyles, eventoMargin } from "../styles/styles";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 export default function Eventos(props) {
   return (
     <div style={eventoMargin}>
       <Link to={`/evento/${props.nombre}`}>
-        <Avatar
-          isBordered
-          radius="md"
-          color="danger"
-          src={props.imagen}
-          style={EventoCardStyles}
-        />
+        <div
+          style={{
+            border: "4px solid #F31260",
+            borderRadius: "10px",
+            overflow: "hidden",
+            width: "140px",
+            height: "140px",
+          }}
+        >
+          <LazyLoadImage
+            alt={props.nombre}
+            src={props.imagen}
+            width={140}
+            height={140}
+            effect="blur"
+            style={{ objectFit: "cover" }}
+            placeholderSrc={props.imagen}
+            useIntersectionObserver={true}
+          />
+        </div>
       </Link>
     </div>
   );
