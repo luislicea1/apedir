f43d1c921479bfe8fc05b64acd6ef0efb9e6a704
ClaudiaQueipo <claudia.queipog@gmail.com>
2023-12-12 15:23:53 -0500

    agregado el mensaje de baneo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
-//import { lugares } from "../components/Lugares/Lugares";
-//import { eventos } from "../components/Lugares/Eventos";
 import Seccion from "../components/Seccion/Seccion";
 import SeccionEventos from "../components/Eventos/SeccionEventos";
 import React from "react";
+import { useHref } from "react-router-dom";
+import { Toaster, toast } from "sonner";
 
 export default function Home() {
+  const history = useHref();
+
+  React.useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const errorParam = urlSearchParams.get("error");
+
+    if (errorParam) {
+      if (errorParam === "unauthorized_client") {
+        toast.error("Error: Usuario baneado.");
+      } else {
+        toast.error("Error desconocido.");
+      }
+    }
+  }, []);
+
   return (
     <div>
-      <SeccionEventos title = "Eventos"></SeccionEventos>
-      <Seccion title="Lugares" ></Seccion>
+      <SeccionEventos title="Eventos"></SeccionEventos>
+      <Seccion title="Lugares"></Seccion>
+      <Toaster richColors theme="dark" duration={3000} position="bottom-center" />
     </div>
   );
 }
