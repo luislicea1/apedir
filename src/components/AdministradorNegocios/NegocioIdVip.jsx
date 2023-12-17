692112d1277ab59ffdf1c6c6488ed5dbb54ed538
luislicea1 <luis.licea.berenguer@gmail.com>
2023-12-16 21:55:50 -0500

    arreglando el css en el vip de nuevo

--- src/components/AdministradorNegocios/NegocioIdVip.jsx
+++ src/components/AdministradorNegocios/NegocioIdVip.jsx
@@ -5,6 +5,7 @@ import { CardStyles, ImgCardStyle, LogoStyle } from "../styles/styles";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { useBussinessStore } from "../../hooks/useStore";
+import './vip.css'
 
 function NegocioIdVip(props) {
   const setBussiness = useBussinessStore((state) => state.setBussiness);
@@ -18,9 +19,9 @@ function NegocioIdVip(props) {
           ? {
               border: "2px solid #5E17EB",
               ...CardStyles,
-              margin: "5px 5px 5px 5px",
+              //margin: "5px 5px 5px 5px",
             }
-          : { ...CardStyles, margin: "5px 5px 5px 5px" }
+          : { ...CardStyles }
       }
     >
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
@@ -56,7 +57,7 @@ function NegocioIdVip(props) {
           alt={props.nombre}
           src={props.imagen}
           effect="blur"
-          style={{ ...LogoStyle, objectFit: "contain" }}
+          style={{ ...LogoStyle, objectFit: "cover" }}
           placeholderSrc={props.imagen}
           useIntersectionObserver={true}
           className="lazyload"
